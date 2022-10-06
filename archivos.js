6bbb383183b46adb97068ebe33892ec4dc9ee2f8
Carlos Julian Lopez <113914323+Caito3@users.noreply.github.com>
2022-10-06 14:01:09 -0300

    Add files via upload

--- archivos.js
+++ archivos.js
@@ -10,23 +10,24 @@ function traerDatosNombre() {
    
    
    document.getElementById("nombrePersonal").innerHTML= nombre;
- 
-   
-   
-      
-}
     
-function nombres(){
-    var nombre = document.getElementById("PonerNombre").value
-   if( nombre == "" ){
+     var nombre = nombre.length
+     
+    if( nombre == 1 ){
       nombrePersonal.textContent= "Nombre y apellidos"
       }
+}
+function codigos(){
+    
+    if (event.keyCode < 45 || event.keyCode > 57) {event.returnValue = false;}
 }
 function traerDatosCod(){
    
     var cod = document.getElementById("PonerCod").value;
     var cod = cod.replace(/\s/g,'')
     var cod = cod.replace(/\D/g,'')
+    
+
     
     var cod = cod.replace(/([0-9]{4})/g,'$1 ')
     document.getElementById("codPersonal").innerHTML= cod;
@@ -62,14 +63,18 @@ function completar(){
     var codCorto = document.getElementById("PonerCvc").value
     
     
-    
     if(nombre == "" || cod == "" || codCorto == ""){
     alert("Completar")
-    
     }
+    
+    
+    
+    
+    
     else{
-        
-     alert("Se recibieron correctamente los datos")
+    var confirmar = document.getElementById("datosPasados")    
+    
+    confirmar.style.display="block"
     }
 
 }
